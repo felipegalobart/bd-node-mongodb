690900d1d15aac0850d53cdb2501f4e8ae53f7e2
Felipe Galobart Jora <felipe@mitsuwa.com.br>
2023-07-24 17:42:54 -0300

    exercicio criar usuários novos

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,32 +1,28 @@
 import { Request, Response } from 'express';
+import User from '../models/User';
 
-export const nome = (req: Request, res: Response) => {
-    let nome: string = req.query.nome as string;
-    let idade: string = req.query.idade as string;
+export const novoUsuario = async (req: Request, res: Response) => {
+  let firstName = req.body.firstName as string;
+  let lastName = req.body.lastName as string;
+  let email = req.body.email as string;
+  let age = req.body.age as number;
+  let interests = req.body.interests.split(',');
 
-    res.render('pages/nome', {
-        nome,
-        idade
-    });
+  let newUser = new User();
+  newUser.name = { firstName: firstName, lastName: lastName };
+  newUser.email = email;
+  newUser.age = age;
+  newUser.interests = interests;
+
+  let saveNewUser = await newUser.save();
+  console.log(saveNewUser);
+  res.redirect('/');
 };
 
-export const idadeForm = (req: Request, res: Response) => {
-    res.render('pages/idade');
-};
+export const buscaUsuario = async (req: Request, res: Response) => {
+  let firstName = req.body.firstName as string;
+  let usersSearch = await User.findOne({ firstName });
 
-export const idadeAction = (req: Request, res: Response) => {
-    let mostrarIdade: boolean = false;
-    let idade: number = 0;
-
-    if(req.body.ano) {
-        let anoNascimento: number = parseInt(req.body.ano as string);
-        let anoAtual: number = new Date().getFullYear();
-        idade = anoAtual - anoNascimento;
-        mostrarIdade = true;
-    }
-
-    res.render('pages/idade', {
-        idade,
-        mostrarIdade
-    });
+  res.render('pages/home', {usersSearch} );
+  res.redirect('/');
 };
